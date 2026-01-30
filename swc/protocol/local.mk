560fbff80bb9b821a6e6ecb2d59087ecc05a00b2
shrub <maybeshrub@gmail.com>
2026-01-30 20:18:57 +0000

    scanner bs

--- swc/protocol/local.mk
+++ swc/protocol/local.mk
@@ -16,13 +16,19 @@ $(dir)_PACKAGES := wayland-server
 
 define protocol_rules
 
-$(dir)/$$(basename $$(notdir $(1)))-protocol.c: $(1)
-	$$(Q_GEN)$$(WAYLAND_SCANNER) code <$$< >$$@
-$(dir)/$$(basename $$(notdir $(1)))-server-protocol.h: $(1)
-	$$(Q_GEN)$$(WAYLAND_SCANNER) server-header <$$< >$$@
+$(dir)/$$(basename $$(notdir $(1)))-protocol.c: $(1) \
+    $(dir)/$$(basename $$(notdir $(1)))-server-protocol.h \
+    $(dir)/$$(basename $$(notdir $(1)))-client-protocol.h
+	$$(Q_GEN)$$(WAYLAND_SCANNER) code <$(1) >$$@
 
-CLEAN_FILES += $(foreach type,protocol.c server-protocol.h, \
-                 $(dir)/$$(basename $$(notdir $(1)))-$(type))
+$(dir)/$$(basename $$(notdir $(1)))-server-protocol.h: $(1)
+	$$(Q_GEN)$$(WAYLAND_SCANNER) server-header <$(1) >$$@
+
+$(dir)/$$(basename $$(notdir $(1)))-client-protocol.h: $(1)
+	$$(Q_GEN)$$(WAYLAND_SCANNER) client-header <$(1) >$$@
+
+CLEAN_FILES += $(foreach type,protocol.c server-protocol.h client-protocol.h, \
+    $(dir)/$$(basename $$(notdir $(1)))-$(type))
 
 endef
 
